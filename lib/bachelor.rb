ea7c5a5dc882f11539c257b78decb6c907c3f95b
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-14 16:52:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -78,7 +78,7 @@ def get_average_age_for_season(data, season)
           end 
           binding.pry
            average = ages.sum/ages.size
-           return average.
+           return average.ceil
            #binding.pry
       end
      
